af9bda8bbe50a7ca6544b2a72c4aceb54c56c694
hoahanie <69376027+hoahanie@users.noreply.github.com>
2020-11-29 22:27:39 +0700

    Add files via upload

---
+++ mevabe.py
@@ -0,0 +1,105 @@
+from __future__ import absolute_import
+import scrapy
+from scrapy import Request
+from scrapy.crawler import CrawlerProcess
+from scrapy.selector import Selector
+from scrapy.item import Item,Field
+import urllib
+import json
+import datetime
+
+class mevabeSpider(scrapy.Spider):
+    name = 'mevabe'
+    headers = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "origin": "https://xe.chotot.com",
+        "pragma": "no-cache",
+        "referer": "https://xe.chotot.com/",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Mobile/15E148 Safari/604.1"
+    }
+
+    base_url = 'https://gateway.chotot.com/v1/public/ad-listing'
+    def start_requests(self):
+        with open('city_name.json','r') as f:
+            data = json.loads(f.read())
+            for city in data:
+                city_id = list(city.keys())[0]
+                temp = city[city_id]
+                seo_url = temp.lower().replace(" ","-")
+                filename = './output/mevabe/' + seo_url+'_' + datetime.datetime.today().strftime('%Y-%m-%d-%H-%M') + '.jsonl'
+                query_string_params ='?cg=11010&region_v2={}&st=s,k&w=1&limit=50&w=1'.format(city_id)
+                yield scrapy.Request(
+                    url=self.base_url + query_string_params,
+                    method='GET',
+                    headers=self.headers,
+                    callback=self.parse_page,
+                    meta= {
+                        'city_id': city_id,
+                        'filename': filename
+                    }
+                )
+    def parse_page(self, response):
+        data = json.loads(response.body)
+        city_id = response.meta["city_id"]
+        filename = response.meta["filename"]
+        total_ads = 0
+        if ('total' in data.keys()):
+            if(int(data['total'])>0):
+                total_ads = data['total']
+            else:
+                return
+        number_page = round(total_ads/50)
+
+        for page in range(1,number_page+1):
+            query_string_params = '?o={}&page={}&cg=11010&region_v2={}&st=s,k&w=1&limit=50&w=1'.format((page-1)*50,page,city_id)
+
+            yield response.follow(
+                    url=self.base_url + query_string_params,
+                    method='GET',
+                    headers=self.headers,
+                    callback=self.parse_ads,
+                    meta = {
+                        "filename": filename
+                    }
+                )
+
+    def parse_ads(self, response):
+        data = json.loads(response.body)
+        filename = response.meta["filename"]
+        for ad in data['ads']:
+            yield response.follow(
+                url=self.base_url+ "/" + str(ad['list_id']),
+                method='GET',
+                headers=self.headers,
+                callback=self.parse_ad_detail,
+                meta = {
+                        "filename": filename
+                    }
+                )
+
+    def parse_ad_detail(self, response):
+        data = json.loads(response.body)
+        filename = response.meta["filename"]
+        dictionary ={
+                'list_id': data['ad']['list_id'],
+                'list_time': data['ad']['list_time'],
+                'name': data['ad']["account_name"],
+                "account_id": data['ad']["account_id"],
+                'phone': data['ad']["phone"],
+                'address': data['ad']["area_name"] + "," + data['ad']["region_name"],
+                'region_id': data['ad']["region_v2"]}
+        with open(filename,"a") as f:
+            f.write(json.dumps(dictionary) + '\n')
+
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(mevabeSpider)
+    process.start()
+
+        
---
+++ muaban.py
@@ -0,0 +1,104 @@
+from __future__ import absolute_import
+import scrapy
+from scrapy import Request
+from scrapy.crawler import CrawlerProcess
+from scrapy.selector import Selector
+import urllib
+import json
+import datetime
+
+import psycopg2
+from sshtunnel import SSHTunnelForwarder
+import sys
+
+
+class muaBanBdsSpider(scrapy.Spider):
+    name = 'muabanbds'
+    headers = {
+        "accept": "application/json",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        "origin": "https://nha.chotot.com",
+        "pragma": "no-cache",
+        "referer": "https://nha.chotot.com/",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+    }
+
+    base_url = 'https://gateway.chotot.com/v1/public/ad-listing'
+    
+    def start_requests(self):
+        with open('city_name.json','r') as f:
+            data = json.loads(f.read())
+            for city in data:
+                city_id = list(city.keys())[0]
+                temp = city[city_id]
+                seo_url = temp.lower().replace(" ","-")
+                filename = './output/muaban/' + seo_url+'_' + datetime.datetime.today().strftime('%Y-%m-%d-%H-%M') + '.jsonl'
+
+                query_string_params = '?region_v2={}&cg=1000&limit=50&st=s,k'.format(city_id)
+                yield scrapy.Request(
+                    url=self.base_url + query_string_params,
+                    method='GET',
+                    headers=self.headers,
+                    callback=self.parse_page,
+                    meta= {
+                        'city_id': city_id,
+                        'filename': filename
+                    }
+                ) 
+
+    def parse_page(self, response):
+        data = json.loads(response.body)
+        city_id = response.meta['city_id']
+        filename = response.meta['filename']
+        total_ads = 0
+        if ('total' in data.keys()):
+            if(int(data['total'])>0):
+                total_ads = data['total']
+            else:
+                return
+        number_page = round(total_ads/50)
+        for page in range(1,number_page+1):
+            query_string_params ='?region_v2={}&cg=1000&limit=50&o={}&st=s,k&page={}'.format(city_id,(page-1)*50,page)
+            yield response.follow(
+                    url=self.base_url + query_string_params,
+                    method='GET',
+                    headers=self.headers,
+                    callback=self.parse_ads,
+                    meta={
+                        'filename': filename
+                    }
+                )
+    def parse_ads(self, response):
+        data = json.loads(response.body)
+        filename = response.meta['filename']
+        for ad in data['ads']:
+            yield response.follow(
+                url=self.base_url+ "/" + str(ad['list_id']),
+                method='GET',
+                headers=self.headers,
+                callback=self.parse_ad_detail,
+                meta={
+                        'filename': filename
+                    }
+                )
+
+    def parse_ad_detail(self, response):
+        data = json.loads(response.body)
+        filename = response.meta['filename']
+        with open(filename ,"a") as f:
+            f.write(json.dumps(data) + '\n')
+
+if __name__ == '__main__':
+    process = CrawlerProcess()
+    process.crawl(muaBanBdsSpider)
+    process.start()
+
+
+
+        
